d84d037168aeb396c5aae778388401f40c9c4511
Waseem Mansour <waseem.mn2002@gmail.com>
2024-01-03 21:47:45 +0400

    test: Move Render BeforeEach

--- screens/internationalPayment/InternationalPayment.test.tsx
+++ screens/internationalPayment/InternationalPayment.test.tsx
@@ -1,18 +1,24 @@
-
-import App from "../../App";
 import {render} from "@testing-library/react-native";
 import InternationalPayment from ".";
+import {GetByQuery} from "@testing-library/react-native/build/queries/make-queries";
+import {TextMatch, TextMatchOptions} from "@testing-library/react-native/build/matches";
+import {CommonQueryOptions} from "@testing-library/react-native/build/queries/options";
 
 
 describe('International Payment Screen', () => {
+	
+	let getByTestId: GetByQuery<TextMatch, CommonQueryOptions & TextMatchOptions>;
+	beforeEach(() => {
+		const utils = render(<InternationalPayment/>);
+		getByTestId = utils.getByTestId;
+	});
+	
 	it('Should have FROM Currency Input', () => {
-		const { getByTestId } = render(<InternationalPayment />);
 		const fromCurrencyInput = getByTestId('from-currency-input');
 		expect(fromCurrencyInput).toBeDefined()
 	})
 	
 	it('Should have TO Currency Input', () => {
-		const { getByTestId } = render(<InternationalPayment />);
 		const fromCurrencyInput = getByTestId('to-currency-input');
 		expect(fromCurrencyInput).toBeDefined()
 	})

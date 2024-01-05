60fad6ffe9836172b80dc244fd8ac3e37c2d0b7c
Waseem Mansour <waseem.mn2002@gmail.com>
2024-01-06 01:01:46 +0400

    test: Test Number Input Currency Format

--- components/NumberInput/NumberInput.test.tsx
+++ components/NumberInput/NumberInput.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from "@testing-library/react-native";
+import { fireEvent, render, waitFor } from "@testing-library/react-native";
 import { NumberInput } from './NumberInput';
 
 
@@ -6,7 +6,7 @@ describe('Number Input', () => {
 	
 	it('Should allow only latin numeric', () => {
     const testInputOne = 'johndoe';
-    const testInputTwo = '5000.75'
+    const testInputTwo = '5000.75';
 
     const { getByTestId } = render(<NumberInput />);
 		
@@ -20,5 +20,18 @@ describe('Number Input', () => {
     expect(numberInput.props.value).toEqual(testInputTwo);
 
 	})
+
+  it('Should accept numeric in currency format', async () => {
+    const testInputThree = '250650.827';
+    const { getByTestId, findByTestId } = render(<NumberInput />);
+    const numberInput = getByTestId('number-input');
+
+    fireEvent.changeText(numberInput, testInputThree);
+    fireEvent(numberInput, 'onEndEditing', { nativeEvent: { text: testInputThree } });
+
+    await waitFor(() => {
+      expect(numberInput.props.value).toEqual('250,650.83');
+    }, { timeout: 500 });
+  })
 
 });

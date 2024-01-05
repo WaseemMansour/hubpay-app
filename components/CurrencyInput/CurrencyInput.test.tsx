6c09d648121702638632817146817db66d089281
Waseem Mansour <waseem.mn2002@gmail.com>
2024-01-05 16:47:08 +0400

    test: Allow Currency Input Custom Label

--- components/CurrencyInput/CurrencyInput.test.tsx
+++ components/CurrencyInput/CurrencyInput.test.tsx
@@ -1,23 +1,25 @@
 import { render } from "@testing-library/react-native";
-import { TextMatch, TextMatchOptions } from "@testing-library/react-native/build/matches";
-import { GetByQuery } from "@testing-library/react-native/build/queries/make-queries";
-import { CommonQueryOptions } from "@testing-library/react-native/build/queries/options";
 import { CurrencyInput } from './CurrencyInput';
 
 
 describe('Currency Input', () => {
 	
-	let getByTestId: GetByQuery<TextMatch, CommonQueryOptions & TextMatchOptions>;
-	beforeEach(() => {
-		const utils = render(<CurrencyInput/>);
-		getByTestId = utils.getByTestId;
-	});
-	
 	it('Should show currency flag and code', () => {
-		const currencyFlag = getByTestId('currency-flag');
+    const { getByTestId } = render(<CurrencyInput />);
+		
+    const currencyFlag = getByTestId('currency-flag');
 		expect(currencyFlag).toBeDefined();
+
 		const currencyCode = getByTestId('currency-code');
 		expect(currencyCode).toBeDefined();
 	})
+
+  it('Should allow custom label', () => {
+    const customLabel = 'No One Currency'
+    const { getByTestId } = render(<CurrencyInput label={customLabel} />);
+
+    const label = getByTestId('currency-input-label');
+    expect(label.props.children).toEqual(customLabel);
+  })
 	
 })
--- components/CurrencyInput/CurrencyInput.tsx
+++ components/CurrencyInput/CurrencyInput.tsx
@@ -1,17 +1,29 @@
-import { TextInput, View } from 'react-native'
+import { Text, TextInput, View } from 'react-native'
 
-export const CurrencyInput = () => {
+type Props = {
+  label?: string
+}
+
+export const CurrencyInput = ({ label = 'Currency' }: Props) => {
 
   return (
     <View>
       <View>
-        <View testID='currency-flag'>
-          Currency Flag
+        <View>
+          <Text testID='currency-input-label'>
+            {label}
+          </Text>
         </View>
-        <View testID='currency-code'>
-          Currency Code
+        <View>
+          <View testID='currency-flag'>
+            Currency Flag
+          </View>
+          <View testID='currency-code'>
+            Currency Code
+          </View>
         </View>
       </View>
+      
       <View>
         <TextInput placeholder='250,000' value='500,000.00' />
       </View>

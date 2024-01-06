65220ac55246e600761f4a35bc4a911e183f7056
Waseem Mansour <waseem.mn2002@gmail.com>
2024-01-07 00:20:16 +0400

    feat: Add List of Supported Currencies

---
+++ lib/constants.ts
@@ -0,0 +1,34 @@
+import { Currency } from '../types';
+
+export const CURRENCIES: Currency[] = [
+  {
+    code: 'AED',
+    image: require('../assets/flags/ae.png'),
+    countryCode: 'ae',
+  },
+  {
+    code: 'EGP',
+    image: require('../assets/flags/eg.png'),
+    countryCode: 'eg',
+  },
+  {
+    code: 'USD',
+    image: require('../assets/flags/us.png'),
+    countryCode: 'us',
+  },
+  {
+    code: 'GBP',
+    image: require('../assets/flags/gb.png'),
+    countryCode: 'uk',
+  },
+  {
+    code: 'CAD',
+    image: require('../assets/flags/ca.png'),
+    countryCode: 'ca',
+  },
+  {
+    code: 'JPY',
+    image: require('../assets/flags/jp.png'),
+    countryCode: 'jp',
+  }
+];

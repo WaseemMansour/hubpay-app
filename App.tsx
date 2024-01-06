0f9ccefd7c51bc9e2a516e6616c952b05348544b
Waseem Mansour <waseem.mn2002@gmail.com>
2024-01-06 04:01:01 +0400

    chore: Add Query Client Provider

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { DismissKeyboardHOC } from './components/HOC/DismissKeyboard';
 import InternationalPayment from "./features/internationalPayment";
 
 export default function App() {
+  
+  const queryClient = new QueryClient({ defaultOptions: { queries: { refetchOnWindowFocus: false } } });
+
   return (
-    
+    <QueryClientProvider client={ queryClient }>
       <View style={styles.container}>
         <StatusBar style="auto" />
         <DismissKeyboardHOC >
           <InternationalPayment />
         </DismissKeyboardHOC>
       </View>
-    
+    </QueryClientProvider>
   );
 }
 

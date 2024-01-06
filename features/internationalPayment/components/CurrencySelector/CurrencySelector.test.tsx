1c26f6e75846e4ac7b36cad831514c42afe05210
Waseem Mansour <waseem.mn2002@gmail.com>
2024-01-07 00:27:57 +0400

    feat: Add Currency Selector Logic

--- features/internationalPayment/components/CurrencySelector/CurrencySelector.test.tsx
+++ features/internationalPayment/components/CurrencySelector/CurrencySelector.test.tsx
@@ -1,11 +1,13 @@
-import { render } from "@testing-library/react-native";
+import { fireEvent, render, waitFor } from "@testing-library/react-native";
 import { CurrencySelector } from './CurrencySelector';
 
 
 describe('Currency Input', () => {
+
+  const changeCurrencyMockCB = jest.fn().mockImplementation(currency => console.log({ currency }));
 	
 	it('Should show currency flag and code', () => {
-    const { getByTestId } = render(<CurrencySelector value={0} onAmountChange={() => {}} />);
+    const { getByTestId } = render(<CurrencySelector defaultCurrencyCode='AED' value={0} onAmountChange={() => {}} />);
 		
     const currencyFlag = getByTestId('currency-flag');
 		expect(currencyFlag).toBeDefined();
@@ -16,10 +18,27 @@ describe('Currency Input', () => {
 
   it('Should allow custom label', () => {
     const customLabel = 'No One Currency'
-    const { getByTestId } = render(<CurrencySelector label={customLabel} value={0} onAmountChange={() => {}} />);
+    const { getByTestId } = render(<CurrencySelector defaultCurrencyCode='AED' label={customLabel} value={0} onAmountChange={() => {}} />);
 
     const label = getByTestId('currency-input-label');
     expect(label.props.children).toEqual(customLabel);
+  })
+
+  it('Should allow user to change currency if option is active', () => {
+    
+    const { getByTestId, findByTestId } = render(<CurrencySelector defaultCurrencyCode='AED' value={0} onAmountChange={() => {}} onCurrencyChange={changeCurrencyMockCB} />);
+
+    const currencySelectorModalTrigger = getByTestId('currency-selector-modal-trigger');
+
+    fireEvent.press(currencySelectorModalTrigger);
+    findByTestId('currency-item-USD').then(element => fireEvent.press(element));
+    // fireEvent.press(usdCurrency);
+
+    waitFor(() => {
+      expect(changeCurrencyMockCB).toHaveBeenCalledWith('USD');
+    }, { timeout: 2000 })
+
+    
   })
 	
 })
--- features/internationalPayment/components/CurrencySelector/CurrencySelector.tsx
+++ features/internationalPayment/components/CurrencySelector/CurrencySelector.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, Pressable, Text, View } from 'react-native';
 import CurrencyInput from 'react-native-currency-input';
+import { CURRENCIES } from '../../../../lib/constants';
 import { CurrencyCode } from '../../../../types';
+import { CurrencyItem } from './CurrencyItem';
+import { CurrencySelectorModal } from './CurrencySelectorModal';
 
 type Props = {
   label?: string,
   value: number,
+  defaultCurrencyCode: CurrencyCode,
   onCurrencyChange?: (currency: CurrencyCode) => void,
   onAmountChange: (amount: number) => void,
 }
 
-export const CurrencySelector = ({ label = 'Currency', value, onCurrencyChange, onAmountChange }: Props) => {
+export const CurrencySelector = ({ 
+  label = 'Currency', 
+  value, 
+  defaultCurrencyCode = 'AED',
+  onCurrencyChange, 
+  onAmountChange 
+}: Props) => {
+
+  const defaultCurrency = CURRENCIES.find((currency) => currency?.code === defaultCurrencyCode) ?? CURRENCIES[0];
+  const [currentCurrency, setCurrency] = useState(defaultCurrency);
+  const [showCurrencySelector, setShowCurrencySelector] = useState<boolean>(false);
+
+  const handleCurrencyChange = (currencyCode: CurrencyCode) => {
+    const currency = CURRENCIES.find((currency) => currency.code === currencyCode);
+    if (currency && onCurrencyChange) {
+      setCurrency(currency);
+      onCurrencyChange(currency.code);
+    }
+    setShowCurrencySelector(false);
+  };
 
   return (
     <View>
@@ -19,18 +43,29 @@ export const CurrencySelector = ({ label = 'Currency', value, onCurrencyChange, 
             {label}
           </Text>
         </View>
+
+
         <View>
-          <View testID='currency-flag'>
-            <Text>
-              Currency Flag
-            </Text>
+          <View>
+            <CurrencyItem currency={currentCurrency} />
           </View>
-          <View testID='currency-code'>
-            <Text>
-              Currency Code
-            </Text>
-          </View>
+          {onCurrencyChange
+            ? (
+
+              <View testID='currency-selector-modal-trigger' style={{ backgroundColor: 'purple'}}>
+
+                <Pressable onPress={ () => {
+                  setShowCurrencySelector(true);
+                } }
+                >
+                  <Image source={ require('../assets/down-arrow.png') } />
+                </Pressable>
+
+              </View>
+            )
+            : null}
         </View>
+
       </View>
       
       <View>
@@ -41,8 +76,14 @@ export const CurrencySelector = ({ label = 'Currency', value, onCurrencyChange, 
           delimiter=","
           separator="."
         />
+        <CurrencySelectorModal
+          baseCurrencyCode='AED'
+          isVisible={ showCurrencySelector }
+          onChange={ handleCurrencyChange }
+          onClose={ () => setShowCurrencySelector(false) }
+        />
       </View>
-      
+
     </View>
   )
 }

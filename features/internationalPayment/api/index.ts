8778dd0be9c035ab1f7fb4e2fe9b56a92fe87325
Waseem Mansour <waseem.mn2002@gmail.com>
2024-01-07 04:17:22 +0400

    fix: Hour s conditional logic

--- features/internationalPayment/api/index.ts
+++ features/internationalPayment/api/index.ts
@@ -5,16 +5,14 @@ export const internationalPaymentApi = {
   getExchangeRate: (base: CurrencyCode, target: CurrencyCode): Promise<ExchangeRate> =>
     new Promise((res) => {
       setTimeout(() => {
-        console.log(`Exchange rate from ${base} to ${target}`);
         res(AED_RATES)
       }, 50)
     }),
   getProcessingTime: (base: CurrencyCode, target: CurrencyCode): Promise<string> =>
     new Promise((res) => {
       setTimeout(() => {
-        console.log(`Exchange rate from ${base} to ${target}`);
         const randomHours = Math.ceil(Math.random() * 10);
-        res(`${randomHours} Hour${randomHours > 1 && 's'}`)
+        res(`${randomHours} Hour${randomHours > 1 ? 's' : ''}`)
       }, 50)
     })
 }
